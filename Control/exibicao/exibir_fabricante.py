a87dc1a2ff77b5fdcdac0d9aeaae336ef3871196
gabriel <gabrielsseislppp@gmail.com>
2026-05-20 21:09:31 -0300

    Papelaria SGE

--- Control/exibicao/exibir_fabricante.py
+++ Control/exibicao/exibir_fabricante.py
@@ -7,18 +7,20 @@ def exibir_dados_fabricante():
     if conexao:
         cursor = conexao.cursor()
 
-        cursor.execute("SELECT id_fabricante, nome_fabrica, cidade, tipo_fabrica in resultados FROM fabricante")
+        cursor.execute("""
+            SELECT id_fabricante, nome_fabrica, cidade, tipo_fabrica
+            FROM fabricante
+        """)
+
         resultados = cursor.fetchall()
 
         print("\nFabricantes:")
 
         for id_fabricante, nome_fabrica, cidade, tipo_fabrica in resultados:
-           print(f"ID: {id_fabricante} | Nome: {nome_fabrica} |  Cidade: {cidade} | Tipo: {tipo_fabrica}")
+            print(f"ID: {id_fabricante} | Nome: {nome_fabrica} | Cidade: {cidade} | Tipo: {tipo_fabrica}")
 
         cursor.close()
         conexao.close()
-    
-
  
 
    
--- Control/main.py
+++ Control/main.py
@@ -1,100 +1,153 @@
 import mysql.connector
 from conexao import conectar
+
 from cadastro.cadastro_fabricante import inserir_dados_fabricante
 from exibicao.exibir_fabricante import exibir_dados_fabricante
+
+
 from cadastro.cadastro_produto import inserir_dados_produto
 from exibicao.exibir_produto import exibir_dados_produto
+
+
 from cadastro.cadastro_fornecedor import inserir_dados_fornecedor
 from exibicao.exibir_fornecedor import exibir_dados_fornecedor
+
+
 from cadastro.cadastro_pedido import inserir_dados_pedido
 from exibicao.exibir_pedido import exibir_dados_pedido
+
+
 from cadastro.cadastro_estoque_cd import inserir_dados_estoque_cd
 from exibicao.exibir_estoque_cd import exibir_dados_estoque_cd
+
+
 from cadastro.cadastro_loja import inserir_dados_loja
 from exibicao.exibir_loja import exibir_dados_loja
+
+
 from cadastro.cadastro_estoque_loja import inserir_dados_estoque_loja
 from exibicao.exibir_estoque_loja import exibir_dados_estoque_loja
+
+
 from cadastro.cadastro_categoria import inserir_dados_categoria
-from exibicao.exibir_categoria import  exibir_dados_estoque_categoria
+from exibicao.exibir_categoria import exibir_dados_estoque_categoria
+
+
 from cadastro.cadastro_movimentacao import inserir_dados_movimentacao
 from exibicao.exibir_movimentacao import exibir_dados_movimentacao
+
+
 from cadastro.cadastro_venda import inserir_dados_vendas
 from exibicao.exibir_venda import exibir_vendas
-from exibicao.exibir_trigger import exibir_logs_estoque
 
+
+from exibicao.exibir_trigger import exibir_logs_estoque
 
 
 while True:
     conexao = conectar()
 
-    print("\n===== PAPELARIA SGE =====")
-    print("\n Bem Vindo(a) Ao Sistema💻")
-    print("0 - Sair❌")
-    print("1 - Fabricante🏭")
-    print("2 - Produto📦")
-    print("3 - Fornecedor🤝")
-    print("4 - Pedido📋")
-    print("5 - Estoque CD🛒")
-    print("6 - Loja🛍️")
-    print("7 - Estoque Loja💲")
-    print("8 - Categoria🗂️")
-    print("9 - Movimentação🚚")
-    print("10 - Venda💸")
-    print("11-Alterações Estoque CD🔄")
+    print("\n========== PAPELARIA SGE ==========")
+    print("\nBem Vindo(a) Ao Sistema 💻")
+
+    print("\n=========== CADASTRAR ===========")
+    print("1  - Cadastrar Fabricante🏭")
+    print("2  - Cadastrar Produto📦")
+    print("3  - Cadastrar Fornecedor🤝")
+    print("4  - Cadastrar Pedido📋")
+    print("5  - Cadastrar Estoque CD🛒")
+    print("6  - Cadastrar Loja🛍️")
+    print("7  - Cadastrar Estoque Loja💲")
+    print("8  - Cadastrar Categoria🗂️")
+    print("9  - Cadastrar Movimentação🚚")
+    print("10 - Cadastrar Venda💸")
+
+    print("\n============ LISTAR ============")
+    print("11 - Listar Fabricantes🏭")
+    print("12 - Listar Produtos📦")
+    print("13 - Listar Fornecedores🤝")
+    print("14 - Listar Pedidos📋")
+    print("15 - Listar Estoque CD🛒")
+    print("16 - Listar Lojas🛍️")
+    print("17 - Listar Estoque Loja💲")
+    print("18 - Listar Categorias🗂️")
+    print("19 - Listar Movimentações🚚")
+    print("20 - Listar Vendas💸")
+    print("21 - Listar Mudanças Estoque CD🔄")
+
+    print("\n0 - Sair❌")
 
     opcao = input("\nEscolha uma opção: ")
 
+    
     if opcao == "1":
-        print("Fabricantes")
         inserir_dados_fabricante()
-        exibir_dados_fabricante()
 
     elif opcao == "2":
         inserir_dados_produto()
-        exibir_dados_produto()
 
     elif opcao == "3":
         inserir_dados_fornecedor()
-        exibir_dados_fornecedor()
 
     elif opcao == "4":
         inserir_dados_pedido()
-        exibir_dados_pedido()
 
     elif opcao == "5":
         inserir_dados_estoque_cd()
-        exibir_dados_estoque_cd()
 
     elif opcao == "6":
         inserir_dados_loja()
-        exibir_dados_loja()
 
     elif opcao == "7":
         inserir_dados_estoque_loja()
-        exibir_dados_estoque_loja()
 
     elif opcao == "8":
         inserir_dados_categoria()
-        exibir_dados_estoque_categoria()
 
     elif opcao == "9":
         inserir_dados_movimentacao()
-        exibir_dados_movimentacao()
 
     elif opcao == "10":
         inserir_dados_vendas()
-        exibir_vendas()
 
     elif opcao == "11":
+        exibir_dados_fabricante()
+
+    elif opcao == "12":
+        exibir_dados_produto()
+
+    elif opcao == "13":
+        exibir_dados_fornecedor()
+
+    elif opcao == "14":
+        exibir_dados_pedido()
+
+    elif opcao == "15":
+        exibir_dados_estoque_cd()
+
+    elif opcao == "16":
+        exibir_dados_loja()
+
+    elif opcao == "17":
+        exibir_dados_estoque_loja()
+
+    elif opcao == "18":
+        exibir_dados_estoque_categoria()
+
+    elif opcao == "19":
+        exibir_dados_movimentacao()
+
+    elif opcao == "20":
+        exibir_vendas()
+
+    elif opcao == "21":
         exibir_logs_estoque()
 
-
-
     elif opcao == "0":
-        print("Sistema encerrado👋.")
+        print("Sistema encerrado 👋")
         break
 
     else:
-        print("Opção inválida👎.")
+        print("Opção inválida 👎")
 
 
